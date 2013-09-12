ddefc85b7962345edeac03de2145e82644b5f9ae
Francois Brochu <francoisbrochu@yahoo.com>
2013-09-12 16:06:05 -0600

    I added a comparison field to IV & Encryption key - if not noted correctly it would brick the file

--- sasel/src/com/eceapp/sasel/IvSetEncryptionKeyActivity.java
+++ sasel/src/com/eceapp/sasel/IvSetEncryptionKeyActivity.java
@@ -28,10 +28,13 @@ public class IvSetEncryptionKeyActivity extends Activity {
 			@Override
 			public void onClick(View v){
 				try{
-					EditText iv_et = (EditText)findViewById(R.id.iv_et);
 					String iv_key = "";
 					String encryptionKey = "";
+					String iv_key2 = "";
+					String encryptionKey2 = "";
 					boolean invalidInput = false;
+
+					EditText iv_et = (EditText)findViewById(R.id.iv_et);					
 					//System.err.println("iv_et is null "+(iv_et == null));
 					if (iv_et != null){
 						iv_key = (iv_et.getText()).toString();
@@ -41,13 +44,44 @@ public class IvSetEncryptionKeyActivity extends Activity {
 							invalidInput = true;
 						}
 					}
-					EditText encryptionKey_et = (EditText)findViewById(R.id.encryption_key_et);
-					
-					//System.err.println("iv_et is null "+(encryptionKey_et == null));
+
+					EditText iv_et2 = (EditText)findViewById(R.id.iv_et2);					
+					//System.err.println("iv_et2 is null "+(iv_et2 == null));
+					if (iv_et2 != null){
+						iv_key2 = (iv_et2.getText()).toString();
+						
+						if ((iv_key2 == null) || ( iv_key2.length() != CipherAlgo.REQ_IV_LEN)){
+							iv_et2.setError("IV Key length must be "+CipherAlgo.REQ_ENCRYPTION_LEN);
+							invalidInput = true;
+						}
+						System.err.println("encryptionKey= "+encryptionKey+" and encryptionKey2 = "+encryptionKey2);
+						if ( !iv_key.equals(iv_key2) ){
+							iv_et2.setError("IV Keys entries does not match = "+iv_key2);
+							invalidInput = true;
+						}
+					}
+
+					EditText encryptionKey_et = (EditText)findViewById(R.id.encryption_key_et);					
+					//System.err.println("encryption_key_et is null "+(encryption_key_et == null));
 					if (encryptionKey_et != null){
 						encryptionKey = (encryptionKey_et.getText()).toString();
 						if ((encryptionKey == null ) || (encryptionKey.length() != CipherAlgo.REQ_ENCRYPTION_LEN)){
 							encryptionKey_et.setError("Encryption key length must be "+CipherAlgo.REQ_ENCRYPTION_LEN);
+							invalidInput = true;
+						}
+					}
+					
+					EditText encryptionKey_et2 = (EditText)findViewById(R.id.encryption_key_et2);					
+					//System.err.println("encryption_key_et2 is null "+(encryption_key_et2 == null));
+					if (encryptionKey_et2 != null){
+						encryptionKey2 = (encryptionKey_et2.getText()).toString();
+						if ((encryptionKey2 == null ) || (encryptionKey2.length() != CipherAlgo.REQ_ENCRYPTION_LEN)){
+							encryptionKey_et2.setError("Encryption key length must be "+CipherAlgo.REQ_ENCRYPTION_LEN);
+							invalidInput = true;
+						}
+						//System.err.println("encryptionKey= "+encryptionKey+" and encryptionKey2 = "+encryptionKey2);
+						if ( !encryptionKey.equals(encryptionKey2) ){
+							encryptionKey_et2.setError("Encryption Keys entries does not match" + encryptionKey2);
 							invalidInput = true;
 						}
 					}
